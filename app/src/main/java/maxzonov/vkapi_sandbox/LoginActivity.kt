0a4709e46d705427c8dc5e8ac82d1fc7b9d34429
MaxZonov <max75758231@yandex.ru>
2018-10-09 20:15:23 +0300

    Update login activity with taken token and user id

--- app/src/main/java/maxzonov/vkapi_sandbox/LoginActivity.kt
+++ app/src/main/java/maxzonov/vkapi_sandbox/LoginActivity.kt
@@ -1,12 +1,18 @@
 package maxzonov.vkapi_sandbox
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.graphics.Bitmap
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -33,12 +39,46 @@ class LoginActivity : AppCompatActivity() {
 
     }
 
-    class VkWebViewClient : WebViewClient() {
+    inner class VkWebViewClient : WebViewClient() {
 
-        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
-            super.onPageStarted(view, url, favicon)
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
 
-            Log.d("myLog", url)
+            parseUrl(url)
         }
+    }
+
+    private fun parseUrl(url: String?) {
+
+        if (url == null) {
+            Toast.makeText(this, "Вернулся неверный url", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (url.startsWith(REDIRECT_URI)) {
+
+            var newUrl = url.replace('#', '?')
+
+            val accessToken: String? = Uri.parse(newUrl).getQueryParameter("access_token")
+            val userId: Long? = Uri.parse(newUrl).getQueryParameter("user_id").toLong()
+
+            Log.d("myLog", "token: $accessToken userId: $userId")
+
+            if (accessToken != null && userId != null) {
+                writeParamsToPrefs(accessToken, userId)
+//                startActivity(Intent(this, StartActivity.class))
+            }
+        } else {
+            setResult(Activity.RESULT_CANCELED)
+            finish()
+        }
+    }
+
+    private fun writeParamsToPrefs(token: String, userId: Long) {
+        val prefs = this.getSharedPreferences("params", Context.MODE_PRIVATE)
+        val editor = prefs.edit()
+        editor.putString("token", token)
+        editor.putLong("userId", userId)
+        editor.apply()
     }
 }
