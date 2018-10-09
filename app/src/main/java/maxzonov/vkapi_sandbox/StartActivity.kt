808fee518fc42df22832bcf0fa2ad5776cc307fd
MaxZonov <max75758231@yandex.ru>
2018-10-09 20:06:14 +0300

    Rename main activity

--- app/src/main/java/maxzonov/vkapi_sandbox/StartActivity.kt
+++ app/src/main/java/maxzonov/vkapi_sandbox/StartActivity.kt
@@ -3,18 +3,16 @@ package maxzonov.vkapi_sandbox
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_start.*
 
-class MainActivity : AppCompatActivity() {
-
+class StartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_start)
 
         btn_sign_in.setOnClickListener {
-            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
     }
