5e6fbe6a7b0c4478142573060e87b6e5a6e3860c
MaxZonov <max75758231@yandex.ru>
2018-11-24 20:47:31 +0300

    Add photo activity preparation code

---
+++ app/src/main/java/maxzonov/vkapi_sandbox/ui/photos/PhotosAdapter.kt
@@ -0,0 +1,36 @@
+package maxzonov.vkapi_sandbox.ui.photos
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_photo.view.*
+import android.view.LayoutInflater
+import maxzonov.vkapi_sandbox.R
+
+
+class PhotosAdapter (val context: Context, val photos: ArrayList<String>) : RecyclerView.Adapter<PhotosAdapter.PhotosViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotosViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_photo, parent, false)
+        return PhotosViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return photos.size
+    }
+
+    override fun onBindViewHolder(holder: PhotosViewHolder, position: Int) {
+        Glide.with(context)
+                .load(photos[position])
+                .into(holder.ivPhoto)
+    }
+
+    class PhotosViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val ivPhoto = itemView.iv_item_photo
+
+    }
+}
