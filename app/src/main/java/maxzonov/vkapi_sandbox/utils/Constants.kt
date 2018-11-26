f08dbdbf14c2d9bba0b94bc7dab7c0c841ceb72b
MaxZonov <max75758231@yandex.ru>
2018-11-26 13:34:06 +0300

    Update vk api version

--- app/src/main/java/maxzonov/vkapi_sandbox/utils/Constants.kt
+++ app/src/main/java/maxzonov/vkapi_sandbox/utils/Constants.kt
@@ -6,7 +6,7 @@ class Constants {
         private const val VK_CLIENT_ID = "6707335"
         const val VK_REDIRECT_URI = "https://oauth.vk.com/blank.html"
         const val VK_AUTHORIZE_URI = "https://oauth.vk.com/authorize?"
-        const val VK_API_VERSION = "5.85"
+        const val VK_API_VERSION = "5.92"
         const val VK_AUTH_URL = "https://oauth.vk.com/authorize?client_id=$VK_CLIENT_ID&display=page&" +
                 "redirect_uri=$VK_REDIRECT_URI&scope=friends&response_type=token&v=$VK_API_VERSION"
         const val VK_AUTH_URL_WITH_REGISTRATION = "https://oauth.vk.com/authorize?client_id=$VK_CLIENT_ID" +
