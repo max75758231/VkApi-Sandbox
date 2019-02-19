21df9108697ffa45b1a56b67a5bf9e887cac6358
MaxZonov <max75758231@yandex.ru>
2019-02-19 20:53:25 +0300

    Refactor hashtag highlighter

--- app/src/main/java/maxzonov/vkapi_sandbox/utils/TextFormatter.kt
+++ app/src/main/java/maxzonov/vkapi_sandbox/utils/TextFormatter.kt
@@ -4,17 +4,22 @@ object TextFormatter {
 
     fun highlightHashtagsInText(text: String): String {
         var resultText: String = text
-        val textWithoutNewlines = resultText.replace("\n\n", " ").replace("\n", " ")
+        var hashtags: ArrayList<String> = getHashtagsArrayFromText(resultText)
+        for (hashtag in hashtags) {
+            resultText = resultText.replace(hashtag, "<font color='#1242B6'>$hashtag</font>")
+        }
+
+        return resultText
+    }
+
+    fun getHashtagsArrayFromText(text: String): ArrayList<String> {
+        val textWithoutNewlines = text.replace("\n\n", " ").replace("\n", " ")
         val splittedText = textWithoutNewlines.split(" ")
         val hashtags: ArrayList<String> = ArrayList()
         for (item in splittedText) {
             if (item.startsWith('#'))
                 hashtags.add(item)
         }
-        for (hashtag in hashtags) {
-            resultText = resultText.replace(hashtag, "<font color='#1242B6'>$hashtag</font>")
-        }
-
-        return resultText
+        return hashtags
     }
 }
